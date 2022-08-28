d9fdc7441af72f7629189f6468f06fe69b7b5b0e
Mattis Zheng <mattiszheng@gmail.com>
2022-08-28 21:02:35 +0800

    add Layout.jsx

--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -4,16 +4,17 @@ import { useState } from "react";
 const Daily = () => {
   // time count
   
-
   // get history
+
   // setState
   let [dailyTasks, setDailyTasks] = useState(["daily1", "daily2"]);
-
+  
+  // sort button
+  
   // add tasks
   // delete tasks
   // update tasks
 
-  // sort
 
   // render Daily
   return (
