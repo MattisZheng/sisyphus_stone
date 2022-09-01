398e6f1dba38e12629280dc8bfa98761fe60850c
Mattis Zheng <mattiszheng@gmail.com>
2022-09-01 14:02:20 +0800

    update Sider

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function getItem(label, key, icon, children) {
 }
 
 const items = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [getItem("Tom", "3"), getItem("Bill", "4"), getItem("Alex", "5")]),
-  getItem("Team", "sub2", <TeamOutlined />, [getItem("Team 1", "6"), getItem("Team 2", "8")]),
-  getItem("Files", "9", <FileOutlined />),
+  // auth
+  getItem("登入", "0", <UserOutlined />),
+  getItem("计划", "1", <PieChartOutlined />),
+  getItem("奖励", "2", <DesktopOutlined />),
+  getItem("历史记录", "3", <TeamOutlined />),
 ];
 
 function App() {
@@ -29,7 +29,6 @@ function App() {
       }}
     >
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="logo" />
         <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={items} />
       </Sider>
       <Layout className="site-layout">
@@ -43,25 +42,7 @@ function App() {
           style={{
             margin: "0 16px",
           }}
-        >
-          <Breadcrumb
-            style={{
-              margin: "16px 0",
-            }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              minHeight: 360,
-            }}
-          >
-            Bill is a cat.
-          </div>
-        </Content>
+        ></Content>
         <Footer
           style={{
             textAlign: "center",
@@ -72,6 +53,6 @@ function App() {
       </Layout>
     </Layout>
   );
-};
+}
 
 export default App;
