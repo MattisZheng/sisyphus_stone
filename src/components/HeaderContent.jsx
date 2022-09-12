774dd925572ba3e28d9cbfd49c49a1f5b31b4090
Mattis Zheng <mattiszheng@gmail.com>
2022-09-12 18:05:08 +0800

    update clock

--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 
-let date = new Date();
+const date = new Date();
+let month = date.getMonth() + 1;
+let day = date.getDate();
 let hours = date.getHours();
 let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-let day = date.getDay();
-let month = date.getMonth();
-let year = date.getFullYear();
 
 const HeaderContent = () => {
   // clock
@@ -16,7 +14,11 @@ const HeaderContent = () => {
     setTime(Date());
   }, 1000);
 
-  return <div>{hours} : { minutes}</div>;
+  return (
+    <div>
+      {month} / {day} {hours} : {minutes}
+    </div>
+  );
 };
 
 export default HeaderContent;
