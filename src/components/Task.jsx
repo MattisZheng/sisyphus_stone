72de73892610aae51a0e8cb8a8404119a1892fda
Mattis Zheng <mattiszheng@gmail.com>
2022-09-13 19:48:40 +0800

    update task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import mock from "../..//mock";
 import { DeleteOutlined } from "@ant-design/icons";
 
 const Task = () => {
-
-  
   return (
     <div>
       checkbox name description(hidden) deadline 分类
+      
       <DeleteOutlined />
     </div>
   );
